b917b2d5c851f56a26d20779d6d4a9ce89a2ba5a
Spencer Pope <spenpo@spenpo.com>
2024-10-04 07:02:55 +0800

    changed sort_column param to post_date in wpb_list_child_pages

--- functions.php
+++ functions.php
@@ -211,9 +211,9 @@ function wpb_list_child_pages() {
       
     if ( is_page() && $post->post_parent )
       
-        $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
+        $childpages = wp_list_pages( 'sort_column=post_date&title_li=&child_of=' . $post->post_parent . '&echo=0' );
     else
-        $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+        $childpages = wp_list_pages( 'sort_column=post_date&title_li=&child_of=' . $post->ID . '&echo=0' );
       
     if ( $childpages ) {
       
